f027e90b2396a91cf513b4f46ec9946adfbf57c3
Nekaerth <lasse.j.lundin@gmail.com>
2019-05-29 18:10:28 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/darksouls3/MainActivity.java
+++ app/src/main/java/com/example/darksouls3/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.darksouls3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Testing af API
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://mugenmonkey.com/api/v0/ds3_weapons";
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response){
+                        Log.e("REST Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Log.e("REST Response", error.toString());
+                    }
+                }
+        );
+        requestQueue.add(objectRequest);
     }
 }
