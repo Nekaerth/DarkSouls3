9ec29cda2296d644b5e5e078b300c4bb08991e53
Nekaerth <lasse.j.lundin@gmail.com>
2019-05-29 16:34:34 +0200

    Imported Volley and gson

--- app/src/main/java/com/example/darksouls3/MainActivity.java
+++ app/src/main/java/com/example/darksouls3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.darksouls3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
     }
 }
