d6f50be74ccaad5401df59fc7b8b89425a849c56
nequilich <nequilich@gmail.com>
2019-05-30 14:18:00 +0200

    Added fragment panes

--- app/src/main/java/com/example/darksouls3/MainActivity.java
+++ app/src/main/java/com/example/darksouls3/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.darksouls3;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handleBottomNavigationView();
 
         //Testing af API
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -41,4 +47,49 @@ public class MainActivity extends AppCompatActivity {
         );
         requestQueue.add(objectRequest);
     }
+
+    private void handleBottomNavigationView() {
+        BottomNavigationView btmNavView = findViewById(R.id.bottom_navigation);
+        btmNavView.setOnNavigationItemSelectedListener(btmNavViewListener);
+        getSupportFragmentManager().beginTransaction().replace(
+                R.id.fragment_container,
+                new HomeFragment()
+        ).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener btmNavViewListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_weapons:
+                            selectedFragment = new WeaponsFragment();
+                            break;
+                        case R.id.nav_spells:
+                            selectedFragment = new SpellsFragment();
+                            break;
+                        case R.id.nav_rings:
+                            selectedFragment = new RingsFragment();
+                            break;
+                        case R.id.nav_armor:
+                            selectedFragment = new ArmorFragment();
+                            break;
+                    }
+
+                    if (selectedFragment == null) {
+                        return false;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(
+                            R.id.fragment_container,
+                            selectedFragment
+                    ).commit();
+                    return true;
+                }
+            };
 }
