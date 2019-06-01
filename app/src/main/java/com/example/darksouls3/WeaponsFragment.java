ad1b5f21794c195757e467eba21222db1e4f2312
Nekaerth <lasse.j.lundin@gmail.com>
2019-06-01 16:47:26 +0200

    Update WeaponsFragment.java

--- app/src/main/java/com/example/darksouls3/WeaponsFragment.java
+++ app/src/main/java/com/example/darksouls3/WeaponsFragment.java
@@ -3,6 +3,7 @@ package com.example.darksouls3;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +14,23 @@ import org.json.JSONObject;
 public class WeaponsFragment extends ListElementFragment {
 
 	private TextView name;
+	private TextView weaponType;
 
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		Log.e("onCreateView", "called");
 		View view = inflater.inflate(R.layout.fragment_weapons, container, false);
 		name = view.findViewById(R.id.name);
+		weaponType = view.findViewById(R.id.weaponType);
 		return view;
 	}
 
 	@Override
 	public boolean showElement(JSONObject element) {
-		name.setText("Black Knight Sword");
+		Log.e("showElement", "called");
+		//name.setText("Black Knight Sword");
+		//weaponType.setText("Great Sword");
 		return false;
 	}
 }
