2db7f8854bf13654771517ac5aa9b3c43cf12397
nequilich <nequilich@gmail.com>
2019-06-01 20:53:44 +0200

    Ring info is now shown

--- app/src/main/java/com/example/darksouls3/RingsFragment.java
+++ app/src/main/java/com/example/darksouls3/RingsFragment.java
@@ -7,15 +7,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RingsFragment extends ListElementFragment {
 
+	private TextView name;
+	private TextView type;
+
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_rings, container, false);
+		View view = inflater.inflate(R.layout.fragment_rings, container, false);
+		findViewsById(view);
+		if (getElement() != null) {
+			updateViews(getElement());
+		}
+		return view;
+	}
+
+	private void updateViews(JSONObject element) {
+		try {
+			name.setText(element.getString("name"));
+			type.setText("Rank "
+				+ element.getString("rank")
+				+ ", "
+				+ element.getString("weight")
+				+ "lbs");
+		} catch (JSONException e) {
+		}
+	}
+
+	private void findViewsById(View view) {
+		name = view.findViewById(R.id.ring_name);
+		type = view.findViewById(R.id.ring_type);
 	}
 
 }
