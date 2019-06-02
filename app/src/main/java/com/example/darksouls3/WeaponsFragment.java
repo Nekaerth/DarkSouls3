c0d3b1a2ea0f393eb6f3dd9c99e0517558afc707
Nekaerth <lasse.j.lundin@gmail.com>
2019-06-02 14:33:32 +0200

    Shield bug fix and id naming fix

--- app/src/main/java/com/example/darksouls3/WeaponsFragment.java
+++ app/src/main/java/com/example/darksouls3/WeaponsFragment.java
@@ -15,6 +15,7 @@ public class WeaponsFragment extends ListElementFragment {
 
 	private TextView name;
 	private TextView details;
+	private TextView leftHeader;
 	private TextView physicalValue;
 	private TextView magicValue;
 	private TextView fireValue;
@@ -37,17 +38,18 @@ public class WeaponsFragment extends ListElementFragment {
 	}
 
 	private void findViewsById(View view) {
-		name = view.findViewById(R.id.armorName);
-		details = view.findViewById(R.id.armorDetails);
-		physicalValue = view.findViewById(R.id.armorPhysicalValue);
-		magicValue = view.findViewById(R.id.armorStrikeValue);
-		fireValue = view.findViewById(R.id.armorSlashValue);
-		lightningValue = view.findViewById(R.id.armorThrustValue);
-		darkValue = view.findViewById(R.id.armorFireValue);
-		strengthValue = view.findViewById(R.id.armorBleedValue);
-		dexterityValue = view.findViewById(R.id.armorPoisonValue);
-		intelligenceValue = view.findViewById(R.id.armorFrostValue);
-		faithValue = view.findViewById(R.id.armorPoiseValue);
+		name = view.findViewById(R.id.weaponName);
+		details = view.findViewById(R.id.weaponDetails);
+		leftHeader = view.findViewById(R.id.weaponLeftHeader);
+		physicalValue = view.findViewById(R.id.weaponPhysicalValue);
+		magicValue = view.findViewById(R.id.weaponMagicValue);
+		fireValue = view.findViewById(R.id.weaponFireValue);
+		lightningValue = view.findViewById(R.id.weaponLightningValue);
+		darkValue = view.findViewById(R.id.weaponDarkValue);
+		strengthValue = view.findViewById(R.id.weaponStrengthValue);
+		dexterityValue = view.findViewById(R.id.weaponDexterityValue);
+		intelligenceValue = view.findViewById(R.id.weaponIntelligenceValue);
+		faithValue = view.findViewById(R.id.weaponFaithValue);
 	}
 
 	private void updateViews(JSONObject element) {
@@ -55,13 +57,25 @@ public class WeaponsFragment extends ListElementFragment {
 			name.setText(element.getString("name"));
 			details.setText(element.getString("weapon_type") + " - " + element.getString("weight") + " lbs");
 
-			String damagesString = element.getString("base_damage");
-			String[] damagesList = damagesString.substring(1).split(", ", 6);
-			physicalValue.setText(damagesList[0].split("\\.")[0]);
-			magicValue.setText(damagesList[1].split("\\.")[0]);
-			fireValue.setText(damagesList[2].split("\\.")[0]);
-			lightningValue.setText(damagesList[3].split("\\.")[0]);
-			darkValue.setText(damagesList[4].split("\\.")[0]);
+			boolean isShield = element.getString("weapon_type").equals("Shield");
+
+			if (isShield) {
+				leftHeader.setText("Absorption");
+				physicalValue.setText(element.getString("physical_def"));
+				magicValue.setText(element.getString("magic_def"));
+				fireValue.setText(element.getString("fire_def"));
+				lightningValue.setText(element.getString("lightning_def"));
+				darkValue.setText(element.getString("dark_def"));
+			} else {
+				leftHeader.setText("Attach Damage");
+				String damagesString = element.getString("base_damage");
+				String[] damagesList = damagesString.substring(1).split(", ", 6);
+				physicalValue.setText(damagesList[0].split("\\.")[0]);
+				magicValue.setText(damagesList[1].split("\\.")[0]);
+				fireValue.setText(damagesList[2].split("\\.")[0]);
+				lightningValue.setText(damagesList[3].split("\\.")[0]);
+				darkValue.setText(damagesList[4].split("\\.")[0]);
+			}
 
 			strengthValue.setText(element.getString("strength_req"));
 			dexterityValue.setText(element.getString("dex_req"));
